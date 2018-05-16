b1d6ca16f25885c8a533c8d9828d64c07acb9964
Timothy Chow <timssss@live.com>
2018-05-16 14:35:23 -0500

    Adding react-redux-jwt-authentication boilerplate
    https://github.com/cornflourblue/react-redux-jwt-authentication-example

---
+++ JournalApp.React/src/_services/user.service.js
@@ -0,0 +1,57 @@
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout,
+    getAll
+};
+
+function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch('http://localhost:61121/api/auth/login', requestOptions)
+        .then(response => {
+            if (!response.ok) { 
+                return Promise.reject(response.statusText);
+            }
+            return response.json();
+        })
+        .then(user => {
+            // login successful if there's a jwt token in the response
+            if (user && user.auth_token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch('/users', requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    if (!response.ok) { 
+        return Promise.reject(response.statusText);
+    }
+
+    return response.json();
+}
